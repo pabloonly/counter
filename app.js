92d7a447b57b15c407e8a33ef460bd0d922d7c23
Praise <bamgbadeoladipupo44@gmail.com>
2022-10-26 22:02:02 +0100

    first commit

--- app.js
+++ app.js
@@ -27,18 +27,25 @@ let dec = document.querySelector('.dec')
 
 function DECREASE (){
     heading.innerText--
+    let body = document.body
+    body.style.backgroundColor = 'yellow'
 }
 
 dec.addEventListener('click' , DECREASE)
 
 function INCREASE (){
     heading.innerText++
+    let body = document.body
+    body.style.backgroundColor = 'green'
 }
 
 inc.addEventListener('click' , INCREASE)
 
 function RESET (){
     heading.innerText = 0
+    let body = document.body
+    body.style.backgroundColor = 'red'
 }
 
 res.addEventListener('click' , RESET)
+
